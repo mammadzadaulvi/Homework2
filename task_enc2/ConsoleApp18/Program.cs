32ac89734b9190b812a34aae8d62662039a9e5ae
mammadzadaulvi <ulvifm@code.edu.az>
2022-05-26 12:53:31 +0400

    encapsulation

---
+++ task_enc2/ConsoleApp18/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student("Alfa", "Betta");
+            st.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            //Console.WriteLine(st.Grades);
+            foreach (var item in st.Grades)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ task_enc2/ConsoleApp18/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp18
+{
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        private int[] _grades;
+
+        public int[] Grades
+        {
+            get
+            {
+                int j = 0;
+                int[] newarray = new int[0];
+                for (int i = 0; i < _grades.Length; i++)
+                {
+                    if (_grades[i] % 2 == 0)
+                    {
+                        Array.Resize(ref newarray, newarray.Length + 1);
+                        newarray[j] = _grades[i];
+                        j++;
+                    }
+                }
+                return newarray;
+            }
+            set
+            {
+                int j = 0;
+                int[] newarray = new int[0];
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (value[i] % 2 == 0)
+                    {
+                        Array.Resize(ref newarray, newarray.Length + 1);
+                        newarray[j] = value[i];
+                        j++;
+                    }
+                }
+                _grades = newarray;
+            }
+        }
+
+        public Student(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+    }
+}
