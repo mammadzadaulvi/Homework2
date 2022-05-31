94491b0733068332b8e6e36e1fad346f58e7d926
mammadzadaulvi <ulvifm@code.edu.az>
2022-05-31 17:34:31 +0400

    static

---
+++ task_static/ConsoleApp21/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student tamerlan = new Student();
+            Console.WriteLine(tamerlan.Age); //1
+
+            Student faiq = new Student();
+            Console.WriteLine(faiq.Age); //2
+
+            Student tural = new Student();
+            Console.WriteLine(tural.Age); //3
+
+            Student emil = new Student();
+            Console.WriteLine(emil.Age); //4
+        }
+    }
+}
---
+++ task_static/ConsoleApp21/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp21
+{
+    class Student
+    {
+        public string Fullname { get; set; }
+        public int Age { get; set; }
+        public static int age { get; set; }
+
+        public Student()
+        {
+            age++;
+            Age = age;
+        }
+    }
+}
