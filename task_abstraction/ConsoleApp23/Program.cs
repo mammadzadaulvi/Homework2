0b21060332e6300be9cd77d49a1fc6a79142e0a4
mammadzadaulvi <ulvifm@code.edu.az>
2022-06-02 17:45:35 +0400

    abstract class

---
+++ task_abstraction/ConsoleApp23/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            
+            user.FullName = "Mammadzada Ulvi";
+            user.Email = "ulvi@code.edu.az";
+            user.Password = "Ulvi1234";
+            Console.WriteLine(user.PasswordChecker());
+            user.ShowInfo();
+        }
+    }
+}
