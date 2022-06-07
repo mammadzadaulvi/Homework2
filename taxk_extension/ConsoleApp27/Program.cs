3d0164ac5a78d181148cc803d900e01c8ba594a6
mammadzadaulvi <ulvifm@code.edu.az>
2022-06-07 18:34:00 +0400

    extension

---
+++ taxk_extension/ConsoleApp27/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[0];
+            arr.Add(1);
+            arr.Add(2);
+            arr.Add(3);
+            arr.Add(4);
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ taxk_extension/ConsoleApp27/Support.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp27
+{
+    static class Support
+    {
+        public static int[] Add(this int[] arr, int num)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = num;
+            return arr;
+        }
+    }
+}
